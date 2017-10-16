2b2623504db2137b31bcbcb05ee6d09e60e6abfa
NickLatkovich <nicklatkovich@gmail.com>
2017-10-16 18:09:03 +0300

    Dynamic plane size

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 
-    uint mapWidth = 10u;
-    uint mapHeight = 10u;
-    Plane plane;
+    public GameObject realPlane;
+
+    uint mapWidth = 10;
+    uint mapHeight = 10;
+    Plane mousePlane;
+    Camera mainCamera;
 
     void Start( ) {
-        plane = new Plane(Vector3.up, new Vector3(mapWidth / 2f, 0, mapHeight / 2f));
+        mousePlane = new Plane(Vector3.up, Vector3.zero);
+        realPlane.transform.position = new Vector3(mapWidth / 2f, 0f, mapHeight / 2f);
+        realPlane.transform.localScale = new Vector3(mapWidth / 10f, 1f, mapHeight / 10f);
+        realPlane.GetComponent<Renderer>( ).material.mainTextureScale = new Vector2(mapWidth / 2f, mapHeight / 2f);
+        mainCamera = Camera.main;
+        mainCamera.transform.position =
+            new Ray(realPlane.transform.position, new Vector3(0f, 1f, -1f)).GetPoint(Mathf.Max(mapWidth, mapHeight));
+        mainCamera.transform.rotation = Quaternion.Euler(45f, 0f, 0f);
     }
 
     void Update( ) {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         float distance;
-        if (plane.Raycast(ray, out distance)) {
+        if (mousePlane.Raycast(ray, out distance)) {
             Vector3 hitPoint = ray.GetPoint(distance);
             int x = Mathf.FloorToInt(hitPoint.x);
             int z = Mathf.FloorToInt(hitPoint.z);
