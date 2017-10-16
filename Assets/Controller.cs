3afe64ddfd7f24e2362885847566db13969f9a2a
NickLatkovich <nicklatkovich@gmail.com>
2017-10-16 19:34:04 +0300

    Camera translation

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -10,16 +10,20 @@ public class Controller : MonoBehaviour {
     uint mapHeight = 10;
     Plane mousePlane;
     Camera mainCamera;
+    float cameraDistance;
+    Vector2 cameraPosition;
+    Vector2 prevMousePosition = Vector2.zero;
 
     void Start( ) {
         mousePlane = new Plane(Vector3.up, Vector3.zero);
         realPlane.transform.position = new Vector3(mapWidth / 2f, 0f, mapHeight / 2f);
         realPlane.transform.localScale = new Vector3(mapWidth / 10f, 1f, mapHeight / 10f);
-        realPlane.GetComponent<Renderer>( ).material.mainTextureScale = new Vector2(mapWidth / 2f, mapHeight / 2f);
+        Material realPlaneMaterial = realPlane.GetComponent<Renderer>( ).material;
+        realPlaneMaterial.mainTextureScale = new Vector2(mapWidth, mapHeight);
+        realPlaneMaterial.mainTextureOffset = new Vector2(0.5f, 0.5f);
         mainCamera = Camera.main;
-        mainCamera.transform.position =
-            new Ray(realPlane.transform.position, new Vector3(0f, 1f, -1f)).GetPoint(Mathf.Max(mapWidth, mapHeight));
-        mainCamera.transform.rotation = Quaternion.Euler(45f, 0f, 0f);
+        cameraDistance = Mathf.Max(mapWidth, mapHeight);
+        cameraPosition = realPlane.transform.position;
     }
 
     void Update( ) {
@@ -29,7 +33,16 @@ public class Controller : MonoBehaviour {
             Vector3 hitPoint = ray.GetPoint(distance);
             int x = Mathf.FloorToInt(hitPoint.x);
             int z = Mathf.FloorToInt(hitPoint.z);
-            transform.position = new Vector3(x + 0.5f, 0.25f, z + 0.5f);
+            transform.position = new Vector3(x + 0.5f, 0, z + 0.5f);
         }
+        Vector3 cameraCenterPosition = new Vector3(cameraPosition.x, 0, cameraPosition.y);
+        mainCamera.transform.position = cameraCenterPosition - new Vector3(0, 0, cameraDistance);
+        mainCamera.transform.rotation = Quaternion.identity;
+        mainCamera.transform.RotateAround(cameraCenterPosition, Vector3.right, 80f);
+        Vector2 mousePosition = Input.mousePosition;
+        if (Input.GetMouseButton(2)) {
+            cameraPosition -= (mousePosition - prevMousePosition) / 256f * cameraDistance;
+        }
+        prevMousePosition = Input.mousePosition;
     }
 }
